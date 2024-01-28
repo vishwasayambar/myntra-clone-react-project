fbb81f2b459c587a86cac5d385912c9a15970f91
Vishwa Sayambar <vishwajitsayambar@gmail.com>
2024-01-28 14:16:41 +0530

    Fix: Summary Table is not showing actual values

--- src/components/BagSummary.jsx
+++ src/components/BagSummary.jsx
@@ -1,35 +1,48 @@
+import { useSelector } from "react-redux";
 
 const BagSummary = () => {
 
-    const item = {
-        totalItem: 2,
-        totalMRP: 345,
-        totalDiscount: 35,
-        finalPayment: 800,
-    }
+  const bag = useSelector(store => store.bag);
+  const getAllItems = useSelector(store => store.items);
+
+  const itemsInBag = getAllItems.filter((item) => {
+    return bag.includes(item.id);
+  });
+
+  let totalItem = bag.length;
+  const CONVENIENCE_FEE = 99;
+  let totalDiscount = 0;
+  let totalMRP = 0;
+
+  itemsInBag.forEach(bagItem => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price;
+  });
+  
+  let finalPayment = (totalMRP - totalDiscount) + CONVENIENCE_FEE; 
 
   return (
     <div className="bag-summary">
       <div className="bag-details-container">
-        <div className="price-header">PRICE DETAILS ({item.totalItem} Items) </div>
+        <div className="price-header">PRICE DETAILS ({totalItem} Items) </div>
         <div className="price-item">
           <span className="price-item-tag">Total MRP</span>
-          <span className="price-item-value">₹{item.totalMRP}</span>
+          <span className="price-item-value">₹{totalMRP}</span>
         </div>
         <div className="price-item">
           <span className="price-item-tag">Discount on MRP</span>
           <span className="price-item-value priceDetail-base-discount">
-            -₹{item.totalDiscount}
+            -₹{totalDiscount}
           </span>
         </div>
         <div className="price-item">
           <span className="price-item-tag">Convenience Fee</span>
-          <span className="price-item-value">₹99</span>
+          <span className="price-item-value">₹{CONVENIENCE_FEE}</span>
         </div>
         <hr />
         <div className="price-footer">
           <span className="price-item-tag">Total Amount</span>
-          <span className="price-item-value">₹{item.finalPayment}</span>
+          <span className="price-item-value">₹{finalPayment}</span>
         </div>
       </div>
       <button className="btn-place-order">
