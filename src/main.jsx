4549d93ee3930d6b5a13fb23eaa9b6f90a68e296
Vishwa Sayambar <vishwajitsayambar@gmail.com>
2024-01-28 13:22:43 +0530

    Import Bootstrap css

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Home from "./routes/Home.jsx";
 import Bag from "./components/Bag.jsx";
 import { Provider } from "react-redux";
 import myntraStore from "./store/index.js";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const router = createBrowserRouter([
   {
